d0631e8417c8131a8635331d61b3c4e9f95a7539
Yasin Khan <yasinkhan500@gmail.com>
2022-06-24 13:46:21 -0700

    stache

---
+++ server/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const PORT = 3001 || process.env.PORT;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
