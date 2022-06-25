5e92a9564c1ccc8290feca43f13d217daefaaeaa
Yasin Khan <yasinkhan500@gmail.com>
2022-06-24 17:46:45 -0700

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
+import { useEffect, useState, useRef } from 'react';
+import { io } from 'socket.io-client';
+import randomWords from 'random-words';
+import { v4 } from 'uuid';
 import './App.css';
 
+const generateRandomWords = () => {
+	const randomNum = (min, max) => {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	};
+
+	return randomWords(randomNum(1, 10)).join(' ');
+};
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [users, setUsers] = useState([]);
+	const [yourInfo, setYourInfo] = useState({
+		userName: '',
+		message: '',
+	});
+
+	const socket = io('http://localhost:3001');
+	const sendRandomMessageRef = useRef(() => {});
+	const usersRef = useRef(null);
+
+	sendRandomMessageRef.current = () => {
+		socket.emit('send_message', {
+			id: v4(),
+			userName: 'anon',
+			message: generateRandomWords(),
+		});
+	};
+
+	const scrollToLastMsg = () => {
+		usersRef?.current?.lastChild?.scrollIntoView({
+			behavior: 'smooth',
+		});
+	};
+
+	useEffect(() => {
+		setInterval(() => {
+			sendRandomMessageRef.current();
+		}, 5000);
+	}, []);
+
+	useEffect(() => {
+		socket.on(
+			'receive_message',
+
+			data => {
+				setUsers(user => [...user, data]);
+			},
+			[socket]
+		);
+		scrollToLastMsg();
+
+		return () => socket.disconnect();
+	}, [socket]);
+
+	const handleChange = e => {
+		setYourInfo(yourInfo => ({
+			...yourInfo,
+			hasChanged: true,
+			[e.target.name]: e.target.value,
+		}));
+	};
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		socket.emit('send_message', {
+			id: v4(),
+			userName: yourInfo.userName,
+			message: yourInfo.message,
+		});
+	};
+
+	return (
+		<div className='app'>
+			<div ref={usersRef}>
+				{users.map(user => (
+					<div key={user.id}>
+						<span style={{ fontWeight: 'bold' }}>{user.userName}:</span>{' '}
+						<span style={{ color: 'red' }}>{user.message}</span>
+					</div>
+				))}
+			</div>
+			<form onSubmit={handleSubmit}>
+				<input
+					type='text'
+					placeholder='Username...'
+					name='userName'
+					value={yourInfo.userName}
+					onChange={handleChange}
+				/>
+				<input
+					type='text'
+					placeholder='Message...'
+					name='message'
+					value={yourInfo.message}
+					onChange={handleChange}
+				/>
+				<button onClick={handleSubmit} type='submit'>
+					Send Message
+				</button>
+			</form>
+		</div>
+	);
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+app.use(cors());
+
 const PORT = 3001 || process.env.PORT;
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+const server = http.createServer(app);
+
+const io = new Server(server, {
+	cors: {
+		origin: 'http://localhost:3000',
+		methods: ['GET', 'POST'],
+	},
+});
+
+io.on('connection', socket => {
+	socket.on('send_message', data => {
+		socket.emit('receive_message', data);
+	});
+});
+
+server.listen(PORT, () => {
+	console.log('SERVER IS RUNNING');
+});
